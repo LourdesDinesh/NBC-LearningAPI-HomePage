be36e82e0fcde4d33f0b67b5b5c55123a4c5d042
LourdesDinesh <lourdesdinesh.s@tringapps.com>
2019-09-25 18:24:48 +0530

    Have added CSS and HTML wrapper to the content. But, gallery still has bugs.

--- NBC HomePage/ViewControllers/NewsDescriptionViewController.m
+++ NBC HomePage/ViewControllers/NewsDescriptionViewController.m
@@ -17,18 +17,48 @@
 
 @implementation NewsDescriptionViewController
 NSString *htmlTemplate = @"<html>"
-"<div id = \"contentBodyHeight\">"
-"<head>"
-"<style type=\"text/css\">"
-"body {"
-"background-color: #ffffff;"
-"font-family: \"Roboto-Regular\"; padding: 0px; margin: 0px;"
-"}"
-"</style>"
-"</head>"
-"<body>%@</body>"
-"</div>"
-"</html>";
+    "<div id = \"contentBodyHeight\">"
+    "<head>"
+    "<style type=\"text/css\">"
+    "body {"
+    "background-color: #ffffff;"
+    "font-family: \"Arimo\"; font-size: 10px; color: #171717;"
+    "font-size: 50px;"
+    "text-align: justify;"
+    "}"
+
+    ".embedded.article ul li span {"
+    "   display: none;"
+    "}"
+
+    //**VIDEO
+    ".embeddedWrapper .videoHeadline {"
+    "color: #171717  !important;"
+    "}"
+
+    ".embeddedWrapper .smallestTitle {"
+    "   color: #777777;"
+    "   line-height: 17px !important;"
+
+    "}"
+
+    //**GAllERY
+    ".embedded.gallery h3.smallTitle a {"
+    "   color: #777777 !important;"
+    "   line-height: 14px !important;"
+    "}"
+
+    ".embedded.vertical h3.smallTitle a {"
+    "   color: #777777 !important;"
+    "   line-height: 14px !important;"
+    "}"
+
+    "</style>"
+    "</head>"
+    "<div class=\"article_wrapper\" style=\"margin:0px 7px 0px 7px;\">"
+    "<body>%@</body>"
+    "</div>"
+    "</html>";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -39,13 +69,12 @@ NSString *htmlTemplate = @"<html>"
     self.thumbnailImageView.image = self.thumbnailImage;
     NSString *modifiedHTML = [NSString stringWithFormat: htmlTemplate, self.contentBody];
     [self.webView loadHTMLString:modifiedHTML baseURL:nil];
-    NSLog(modifiedHTML);
-    [self.activityView stopAnimating];
-    [self.activityView setHidden:true];
+    NSLog(@"%@", modifiedHTML);
 }
 
 - (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
     [self.activityView stopAnimating];
+    [self.activityView setHidden:true];
 }
 
 @end
